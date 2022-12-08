047298b30b9e1d33be6811219a08bb85206a5adb
coji <coji@techtalk.jp>
2022-12-08 15:13:23 +0900

    pulls

--- hello.ts
+++ hello.ts
@@ -17,7 +17,7 @@ const buildToken = async (payload: object) => {
 /**
  * アクセストークンを取得する
  */
-const main = async () => {
+const requestAccessToken = async () => {
   const payload = {
     iat: dayjs().unix() - 10, // ちょっと前にすると無効になりにくいらしい。
     exp: dayjs().add(10, 'minutes').unix(), // 10分間だけ有効
@@ -36,6 +36,28 @@ const main = async () => {
     },
   )
 
-  console.log(await ret.json())
+  // アクセストークンを取得
+  const { token: accessToken } = await ret.json()
+  return accessToken
+}
+
+const main = async () => {
+  // アクセストークンを取得
+  const accessToken = await requestAccessToken()
+
+  // 最新のプルリクエストを取得
+  const pulls = await fetch(
+    `https://api.github.com/repos/coji/upflow/pulls?state=all&per_page=1`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `Bearer ${accessToken}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    },
+  )
+
+  console.log(await pulls.json())
 }
 main()
